4c2acbcadb9edad53f14e2751e35eda1cb881284
duantangao <1257511300@qq.com>
2021-01-29 20:55:02 +0800

    11.配置Message全局弹框组件

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import "./assets/fonts/iconfont.css";
 // 导入全局样式
 import "./assets/css/global.css";
 Vue.config.productionTip = false;
-
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+Vue.prototype.$http = axios;
 new Vue({
   router,
   render: (h) => h(App),
