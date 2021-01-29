e4b8356e994414eb41d4606577c3a8b7928ca2f6
duantangao <1257511300@qq.com>
2021-01-29 21:41:50 +0800

    22.侧边栏的折叠与展开效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,12 +11,16 @@
     <!-- 页面主体 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
         <el-menu
           background-color="#333744"
           text-color="#fff"
           active-text-color="#409eff"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
         >
           <!-- 一级菜单 -->
           <el-submenu
@@ -66,6 +70,7 @@ export default {
         102: "iconfont icon-danju",
         145: "iconfont icon-baobiao",
       },
+      isCollapse: false,
     };
   },
   created() {
@@ -81,6 +86,10 @@ export default {
       const { data: res } = await this.$http.get("menus");
       if (res.meta.status !== 200) this.$message.error(res.meta.msg);
       this.menulist = res.data;
+    },
+    // 切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
     },
   },
 };
@@ -108,11 +117,23 @@ export default {
 }
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
 .el-main {
   background-color: #eaedf1;
 }
 .iconfont {
   margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
