98953f54d76230dee05e29dbd10151210328bdaa
duantangao <1257511300@qq.com>
2021-01-29 21:28:54 +0800

    17.侧边栏结构梳理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -9,6 +9,10 @@ import {
   Container,
   Aside,
   Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
 } from "element-ui";
 
 Vue.use(Button);
@@ -19,6 +23,10 @@ Vue.use(Header);
 Vue.use(Container);
 Vue.use(Aside);
 Vue.use(Main);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItemGroup);
+Vue.use(MenuItem);
 
 // 把弹框组件挂着到了 vue 的原型对象上，这样每一个组件都可以直接通过 this 访问
 Vue.prototype.$message = Message;
