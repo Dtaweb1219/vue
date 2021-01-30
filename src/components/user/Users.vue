d57fa3fbe0081610d9b4d81b29e192ccd0f69f13
duantangao <1257511300@qq.com>
2021-01-30 11:06:26 +0800

    32.修改银行列表状态功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -27,7 +27,10 @@
         <el-table-column label="角色" prop="role_name"></el-table-column>
         <el-table-column label="状态">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state"></el-switch>
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="userStateChanged(scope.row)"
+            ></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
@@ -110,6 +113,16 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage;
       this.getUserList();
+    },
+    async userStateChanged(userinfo) {
+      const { data: res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      );
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state;
+        return this.$message.error("更新用户状态失败!");
+      }
+      this.$message.success("更新用户状态成功!");
     },
   },
 };
