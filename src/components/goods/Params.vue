483fa77979098fcb9b165892781ac98540f288d3
duantangao <1257511300@qq.com>
2021-02-02 21:36:12 +0800

    79.渲染参数下的可选项

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -43,7 +43,16 @@
           <!-- 动态参数表格 -->
           <el-table :data="manyTableData" border stripe>
             <!-- 展开行的操作 -->
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <el-tag
+                  v-for="(item, i) in scope.row.attr_vals"
+                  :key="i"
+                  closable
+                  >{{ item }}</el-tag
+                >
+              </template>
+            </el-table-column>
             <!-- 索引列 -->
             <el-table-column type="index"></el-table-column>
             <el-table-column
@@ -235,6 +244,11 @@ export default {
           },
         }
       );
+      // 对参数下的可选项数据进行加工
+      res.data.forEach((item) => {
+        // ''.split(' ') => ['']
+        item.attr_vals = item.attr_vals ? item.attr_vals.split(" ") : [];
+      });
       if (res.meta.status !== 200) {
         return this.$message.error("获取参数列表失败");
       }
