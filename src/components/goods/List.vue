0d904e4ac5eb51e151cdb2f1ee86bdbe9f0b03e3
duantangao <1257511300@qq.com>
2021-02-03 10:43:22 +0800

    86.自定义格式化时间的全局过滤器

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -31,11 +31,11 @@
           prop="goods_weight"
           width="70"
         ></el-table-column>
-        <el-table-column
-          label="创建时间"
-          prop="add_time"
-          width="140"
-        ></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dateFormat }}
+          </template>
+        </el-table-column>
         <el-table-column label="操作" width="130">
           <template slot-scope="">
             <div>
